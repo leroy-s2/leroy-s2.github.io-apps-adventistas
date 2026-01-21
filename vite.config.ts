7db85c48f1b23d64c61a0a6ae88c683b73cdfdbd
leroy-s2 <angel.silva.s@upeu.edu.pe>
2026-01-21 13:00:06 -0500

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Blog-adventist/', // Para GitHub Pages
+  base: '/leroy-s2.github.io-apps-adventistas/', // Para GitHub Pages
 })
