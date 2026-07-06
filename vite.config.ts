0f488c3ced42005a4daef19035266374b6277134
leroy-s2 <angel.silva.s@upeu.edu.pe>
2026-07-06 16:27:09 -0500

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/leroy-s2.github.io-apps-adventistas/', // Para GitHub Pages
+  base: '/', // Para Vercel (servido en la raíz del dominio)
 })
